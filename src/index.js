c4ada1de011cc5e68c67b5661d869b8788af7d5d
Esdras Clother <esdras.clother@outlook.com>
2021-05-12 12:03:02 -0600

    adding login form

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Usuario from './components/Usuario';
+import FormularioInicioSesion from './components/FormularioInicioSesion'
 
 const App = () => {
-  const [sesion, cambiarEstadoSesion] = useState(true);
+  const [sesion, cambiarEstadoSesion] = useState(false);
 
   return (
     <>
-      {sesion === true ? 
-      <div>
-        <Usuario />
-        <button onClick={() => cambiarEstadoSesion(false)}>Cerrar Sesion</button>
-      </div> 
-      : 
-      <div>
-        <p>No has iniciado sesion</p>
-        <button onClick={() => cambiarEstadoSesion(true)}>Iniciar Sesion</button>  
-      </div>
+      {sesion === true ?
+        <div>
+          <Usuario />
+          <button onClick={() => cambiarEstadoSesion(false)}>Cerrar Sesion</button>
+        </div>
+        :
+        <div>
+          <p>No has iniciado sesion</p>
+          <FormularioInicioSesion />
+        </div>
       }
     </>
   );
